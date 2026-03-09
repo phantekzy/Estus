088ac38ce56dea9d93a8b8eaa45b155a6ecd1f7b
phantekzy <mainilotfi@gmail.com>
2026-03-09 21:40:03 +0100

    Redis connection finished

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -1,3 +1,8 @@
-import Redis from "ioredis";
+import { Redis } from "ioredis";
+import "dotenv/config";
 
-export const connection = new Redis()
+export const connection = new Redis({
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: Number(process.env.REDIS_PORT) || 6379,
+  maxRetriesPerRequest: null,
+});
