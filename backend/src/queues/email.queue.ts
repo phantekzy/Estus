a0dff70662624b1cbf74244456ede7d00f783a35
phantekzy <mainilotfi@gmail.com>
2026-03-09 22:22:24 +0100

    email queue

--- backend/src/queues/email.queue.ts
+++ backend/src/queues/email.queue.ts
@@ -1,1 +1,9 @@
+import { Queue } from "bullmq";
+import { EmailJobPayload } from "../types/index.js";
+import { redisConfig } from "../config/redis.js";
+
 const QUEUE_NAME = "email_tasks";
+
+export const emailQueue = new Queue<EmailJobPayload>(QUEUE_NAME, {
+  connection: redisConfig,
+});
