0c3fc94f20497f1fc945dcff038c1ccefe2c149f
phantekzy <mainilotfi@gmail.com>
2026-03-10 08:45:50 +0100

    Defining the queue and the background worker process done

--- backend/src/queues/email.queue.ts
+++ backend/src/queues/email.queue.ts
@@ -1,6 +1,6 @@
-import { Job, Queue, Worker } from "bullmq";
-import { EmailJobPayload } from "../types/index.js";
+import { Queue, Worker, type Job } from "bullmq";
 import { redisConfig } from "../config/redis.js";
+import { EmailJobPayload } from "../types/index.js";
 
 const QUEUE_NAME = "email-tasks";
 
@@ -12,12 +12,16 @@ export const emailWorker = new Worker<EmailJobPayload>(
   QUEUE_NAME,
   async (job: Job<EmailJobPayload>) => {
     const { email, content } = job.data;
-    console.log(`[Worker] Processing Job ${job.id} targeting {email}`);
+    console.log(`[Worker] Processing Job ${job.id} targeting ${email}`);
 
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    console.log(`[Workder] Job ${job.id} execution completed`);
+    console.log(`[Worker] Job ${job.id} execution completed.`);
     return { status: "success" };
   },
   { connection: redisConfig },
 );
+
+emailWorker.on("failed", (job, err) => {
+  console.error(`[Worker] Job ${job?.id} failed: ${err.message}`);
+});
