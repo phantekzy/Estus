25fe8db4df673df174a44349dc753f12c998a838
phantekzy <mainilotfi@gmail.com>
2026-03-10 21:03:14 +0100

    pushing to redis

--- backend/src/controllers/job.controller.ts
+++ backend/src/controllers/job.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import z from "zod";
-import { ApiResponse } from "../types/index.js";
+import { ApiResponse, EmailJobPayload } from "../types/index.js";
+import { emailQueue } from "../queues/email.queue.js";
 
 const JobSchema = z.object({
   email: z.email({ error: "Please Enter a valid email" }),
@@ -10,4 +11,12 @@ const JobSchema = z.object({
 export const dispatchTask = async (
   req: Request,
   res: Response<ApiResponse>,
-): Promise<any> => {};
+): Promise<any> => {
+  try {
+    const payLoad: EmailJobPayload = JobSchema.parse(req.body);
+    const job = await emailQueue.add("process-email", payLoad, {
+      attempts: 3,
+      backoff: { type: "exponential", delay: 1000 },
+    });
+  } catch (error) {}
+};
