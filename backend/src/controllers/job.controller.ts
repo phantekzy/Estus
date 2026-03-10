4d68c2cff1043dbee8cc072442492dfbc5295f2e
phantekzy <mainilotfi@gmail.com>
2026-03-10 20:57:13 +0100

    dispatchTask

--- backend/src/controllers/job.controller.ts
+++ backend/src/controllers/job.controller.ts
@@ -1,6 +1,13 @@
+import { Request, Response } from "express";
 import z from "zod";
+import { ApiResponse } from "../types/index.js";
 
-const jobSchema = z.object({
-  email: z.string().email(),
-  content: z.string().min(5),
+const JobSchema = z.object({
+  email: z.email({ error: "Please Enter a valid email" }),
+  content: z.string().min(5, { error: "Email content is very short " }),
 });
+
+export const dispatchTask = async (
+  req: Request,
+  res: Response<ApiResponse>,
+): Promise<any> => {};
