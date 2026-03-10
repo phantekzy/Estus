645451a8793ac3f3f92598a9ae300484f041bbca
phantekzy <mainilotfi@gmail.com>
2026-03-10 21:20:55 +0100

    completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,14 @@
 import express from "express";
+import cors from "cors";
+import jobRoutes from "./routes/job.routes.js";
+import "dotenv/config";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/jobs", jobRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`[Estus Engine] Online and listening on port ${PORT}`);
+});
