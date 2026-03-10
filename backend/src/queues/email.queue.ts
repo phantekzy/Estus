01119b0281e0c5e5bbbbb86d46d2a14307e2fdeb
phantekzy <mainilotfi@gmail.com>
2026-03-10 08:29:02 +0100

    emailWorker

--- backend/src/queues/email.queue.ts
+++ backend/src/queues/email.queue.ts
@@ -2,7 +2,7 @@ import { Job, Queue, Worker } from "bullmq";
 import { EmailJobPayload } from "../types/index.js";
 import { redisConfig } from "../config/redis.js";
 
-const QUEUE_NAME = "email_tasks";
+const QUEUE_NAME = "email-tasks";
 
 export const emailQueue = new Queue<EmailJobPayload>(QUEUE_NAME, {
   connection: redisConfig,
@@ -12,6 +12,12 @@ export const emailWorker = new Worker<EmailJobPayload>(
   QUEUE_NAME,
   async (job: Job<EmailJobPayload>) => {
     const { email, content } = job.data;
-    console.log(`[Worker] Processing Job ${job.id} targeting ${email}`);
+    console.log(`[Worker] Processing Job ${job.id} targeting {email}`);
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    console.log(`[Workder] Job ${job.id} execution completed`);
+    return { status: "success" };
   },
+  { connection: redisConfig },
 );
