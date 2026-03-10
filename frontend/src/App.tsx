4127ba22a5996c870f52d80274704daa9ae1d9b2
phantekzy <mainilotfi@gmail.com>
2026-03-10 22:18:55 +0100

    Application Entry Points

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import Dashboard from "./features/Dashboard"
+
+const App = () => {
+    return (
+        <main className="min-h-screen flex items-center justify-center p-4">
+            <Dashboard />
+        </main>
+    )
+}
+
+export default App
