8966e23b21d565ef76c4988667d7fe487572e439
phantekzy <mainilotfi@gmail.com>
2026-03-12 04:01:51 +0100

    fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import Dashboard from "./features/Dashboard"
+import { Dashboard } from "./features/Dashboard"
 
 const App = () => {
     return (
--- frontend/src/features/Dashboard.tsx
+++ frontend/src/features/Dashboard.tsx
@@ -1,7 +1,11 @@
 export const Dashboard: React.FC = () => {
     return (
-        <section>
-
+        <section className="w-full max-w-lg bg-surface border border-slate-800 p-8 rounded-2xl shadow-2xl">
+            <div className="mb-8">
+                <h1 className="text-2xl font-bold text-primary tracking-tighter">
+                    Estus
+                </h1>
+            </div>
         </section>
     )
 }
