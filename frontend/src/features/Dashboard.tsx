c7f765189906de0edce4b372c3a9185f4d7be5aa
phantekzy <mainilotfi@gmail.com>
2026-03-12 04:11:19 +0100

    top section done

--- frontend/src/features/Dashboard.tsx
+++ frontend/src/features/Dashboard.tsx
@@ -2,9 +2,12 @@ export const Dashboard: React.FC = () => {
     return (
         <section className="w-full max-w-lg bg-surface border border-slate-800 p-8 rounded-2xl shadow-2xl">
             <div className="mb-8">
-                <h1 className="text-2xl font-bold text-primary tracking-tighter">
+                <h1 className="text-2xl uppercase font-bold text-primary tracking-tighter">
                     Estus
                 </h1>
+                <p className="text-slate-500 text-xs uppercase font-semibold mt-1 ">
+                    Background Worker Dispatcher
+                </p>
             </div>
         </section>
     )
