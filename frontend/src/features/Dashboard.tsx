a9f24e1eaf9507fc6eacf6dbade9a928ec8eaadb
phantekzy <mainilotfi@gmail.com>
2026-03-12 18:46:53 +0100

    handleSubmit

--- frontend/src/features/Dashboard.tsx
+++ frontend/src/features/Dashboard.tsx
@@ -1,8 +1,14 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 export const Dashboard: React.FC = () => {
     const [status, setStatus] = useState<{ type: 'success' | 'error', msg: string } | null>(null);
     const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setIsSubmitting(true);
+        setStatus(null);
+    }
     return (
         <section className="w-full max-w-lg bg-surface border border-slate-800 p-8 rounded-2xl shadow-2xl">
             <div className="mb-8">
